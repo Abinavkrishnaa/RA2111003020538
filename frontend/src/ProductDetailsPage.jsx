87c232ee7edcb12cdc60b95c40a8493a1ee0328a
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-04-03 20:40:03 +0530

    edited

--- frontend/src/ProductDetailsPage.jsx
+++ frontend/src/ProductDetailsPage.jsx
@@ -27,8 +27,7 @@ function ProductDetailsPage() {
         <p>Category: {product.category}</p>
         <p>Price: ${product.price}</p>
         <p>Rating: {product.rating}</p>
-        <p>Discount: {product.discount}%</p>
-        <p>Availability: {product.availability}</p>
+        
       </div>
     </div>
   );
